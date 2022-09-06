a51e5e958b0367afa4ae35eef39a73139099b3fa
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-09-06 03:34:52 -0700

    testing commits

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,21 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+151.times do
+    pokemon = Pokemon.create(
+        name: Faker::Games::Pokemon.name,
+        location: Faker::Games::Pokemon.location,
+        move: Faker::Games::Pokemon.move
+        
+    )
+
+    rand(1..7).times do
+        Review.create(
+            rating: rand(1..10),
+            text: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+            pokemon_id: pokemon.id
+        )
+    end
+end
 
 puts "✅ Done seeding!"
